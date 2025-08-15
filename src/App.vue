f0a2caea9f537c8329320268313b3a260d82e4f1
dasarath ayer <dasarathayer4@gmail.com>
2025-08-15 15:44:31 +0545

    new commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<Header/>
+
+
+<conditional/>
+
+<List/>
+
+
+
+</template>
+
+<script>
+import Header from './components/AppHeader.vue'
+import Conditional from './components/ConditonalRender.vue'
+import List from './components/ListRendring.vue'
+
+export default {
+  name: "App",
+  components:{
+    Header,
+    Conditional,
+    List
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+
+}
+</style>
